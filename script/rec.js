efcf5966e2bd4d2a2acba753e831839180d99968
Rahiji9 <aliahmedrahi007@gmail.com>
2024-02-14 11:13:07 +0600

    geometry frist

---
+++ script/rec.js
@@ -0,0 +1,13 @@
+function calculateRectangleArea() {
+  // get rectangle width velue
+  const rectangleWidthInput=document.getElementById('rectangle-width')
+  const rectangleWidthValue=rectangleWidthInput.value;
+  const width=parseFloat(rectangleWidthValue)
+  console.log(width);
+  // get rectangle lenght velue
+  const rectangleLengthInput=document.getElementById('rectangle-length')
+  const rectangleLengthValue=rectangleLengthInput.value;
+  const length=parseFloat(rectangleLengthValue)
+  console.log(length);
+  
+}
---
+++ script/tri.js
@@ -0,0 +1,13 @@
+function calculateTriangleArea() {
+  // get triangle base value
+  const triangleBaseInput = document.getElementById('triangle-base')
+  const triangleBaseValue = triangleBaseInput.value;
+  const base =parseFloat(triangleBaseValue)
+  console.log(base);
+// get triangle hight value
+  const triangleHightInput = document.getElementById('triangle-hight')
+  const triangleHightValue = triangleHightInput.value;
+  const hight =parseFloat(triangleHightValue)
+  console.log(hight);
+  
+}
