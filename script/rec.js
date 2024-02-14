2ea1129e2bfb4dd568dc43fb264b6195e03bea4a
Rahiji9 <aliahmedrahi007@gmail.com>
2024-02-14 12:11:00 +0600

    sec commit

--- script/rec.js
+++ script/rec.js
@@ -1,13 +1,21 @@
 function calculateRectangleArea() {
   // get rectangle width velue
-  const rectangleWidthInput=document.getElementById('rectangle-width')
-  const rectangleWidthValue=rectangleWidthInput.value;
-  const width=parseFloat(rectangleWidthValue)
+  const rectangleWidthInput = document.getElementById('rectangle-width')
+  const rectangleWidthValue = rectangleWidthInput.value;
+  const width = parseFloat(rectangleWidthValue)
   console.log(width);
   // get rectangle lenght velue
-  const rectangleLengthInput=document.getElementById('rectangle-length')
-  const rectangleLengthValue=rectangleLengthInput.value;
-  const length=parseFloat(rectangleLengthValue)
+  const rectangleLengthInput = document.getElementById('rectangle-length')
+  const rectangleLengthValue = rectangleLengthInput.value;
+  const length = parseFloat(rectangleLengthValue)
   console.log(length);
-  
+
+
+  // rectangle area calculate
+  const area = length * width
+  console.log(area);
+
+  // display rectangle area
+const rectangleAreaSpan = document.getElementById('rectangle-area')
+rectangleAreaSpan.innerText =area;
 }
